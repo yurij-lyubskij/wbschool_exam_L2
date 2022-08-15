04776e4ec162a79e206d29e1dc6f0996549e598a
yura <yura11011@yandex.com>
2022-08-15 17:51:07 +0300

    task 3

--- develop/dev03/task.go
+++ develop/dev03/task.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -35,9 +36,30 @@ import (
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
+var usageStr = `
+Usage: sort [options] <input file> <output file>
+output file - stdout по умолчанию
+Options:
+-k — указание колонки для сортировки
+-n — сортировать по числовому значению
+-r — сортировать в обратном порядке
+-u — не выводить повторяющиеся строки
+`
+
+func usage() {
+	fmt.Printf("%s\n", usageStr)
+	os.Exit(0)
+}
+
+// функция, которая возвращает
+//функцию less для slice.Sort для конкретного слайса
+//сортирует в нужном порядке, при передаче флага - в обратном порядке
 type sliceSort func(fSlice []string, rev bool) func(i int, j int) bool
 
-func readFile(fileName string) ([]string, error) {
+//ReadFile - читает файл по передаваемому имени,
+//возвращает слайс строк и ошибку
+//если имя пустое, читает из stdin
+func ReadFile(fileName string) ([]string, error) {
 	var sc *bufio.Scanner
 	if len(fileName) == 0 {
 		sc = bufio.NewScanner(os.Stdin)
@@ -63,10 +85,19 @@ func readFile(fileName string) ([]string, error) {
 	return fSlice, nil
 }
 
-func writeFile(fileName string, fSlice []string) error {
-	f, err := os.Create(fileName)
-	if err != nil {
-		return err
+//WriteFile - пишет файл по передаваемому имени,
+//возвращает ошибку при необходимости
+//если имя пустое, пишет в Stdout
+func WriteFile(fileName string, fSlice []string) error {
+	var f *os.File
+	var err error
+	if len(fileName) == 0 {
+		f = os.Stdout
+	} else {
+		f, err = os.Create(fileName)
+		if err != nil {
+			return err
+		}
 	}
 	for _, s := range fSlice {
 		_, err := fmt.Fprintln(f, s)
@@ -74,6 +105,7 @@ func writeFile(fileName string, fSlice []string) error {
 			return err
 		}
 	}
+
 	f.Close()
 	return nil
 }
@@ -91,23 +123,30 @@ func order(i, j string) bool {
 	return a < b
 }
 
-func xor(a, b bool) bool {
-	return (a && !b) || (!a && b)
-}
-
+//alphabetSort - функция, которая возвращает
+//функцию less для slice.Sort для конкретного слайса
+//сортирует по алфавиту, при передаче флага - в обратном порядке
 func alphabetSort(fSlice []string, rev bool) func(i, j int) bool {
 	return func(i, j int) (res bool) {
 		defer func() {
-			res = xor(res, rev)
+			if rev {
+				res = !res
+			}
 		}()
 		return order(fSlice[i], fSlice[j])
 	}
 }
 
+// numSort- функция, которая возвращает
+//функцию less для slice.Sort для конкретного слайса
+//сортирует по величине числа (numeric),
+//при передаче флага - в обратном порядке
 func numSort(fSlice []string, rev bool) func(i, j int) bool {
 	return func(i, j int) (res bool) {
 		defer func() {
-			res = xor(res, rev)
+			if rev {
+				res = !res
+			}
 		}()
 		a, err1 := strconv.Atoi(fSlice[i])
 		b, err2 := strconv.Atoi(fSlice[j])
@@ -124,6 +163,14 @@ func numSort(fSlice []string, rev bool) func(i, j int) bool {
 	}
 }
 
+//getColumnMap - функция, которая принимет
+//разделитель и номер колонки
+//разделяет строки на колонки,
+//выделяет нужную колонку
+//возвращает слайс с нужной колонкой
+//мапу, где ключ - нужная колонка,
+//значение - исходная строка
+//и слайс строк, где нет нужной колонки
 func getColumnMap(fSlice []string, sep string, k int) (StringMap map[string][]string, keys []string, unsorted []string) {
 	StringMap = make(map[string][]string)
 	for _, str := range fSlice {
@@ -141,7 +188,9 @@ func getColumnMap(fSlice []string, sep string, k int) (StringMap map[string][]st
 	return
 }
 
-func Unique(input []string) []string {
+//unique - функция, которая убирает
+//повторяющиеся строки
+func unique(input []string) []string {
 	StringSet := make(map[string]struct{})
 	for _, str := range input {
 		_, ok := StringSet[str]
@@ -158,6 +207,13 @@ func Unique(input []string) []string {
 	return uniq
 }
 
+//sortByColumn - функция, которая сортирует
+//строки по значению в нужной колонке
+//значения, где нет требуемой колонки
+//идут в начале, по значению в нужной
+//колонке сортируем функцией mainSort
+//при совпадении ключей или отсутствии колонки
+//сортируем функцией subSort
 func sortByColumn(fSlice []string, k int, mainSort, subSort sliceSort, reversal bool) []string {
 	StringMap, keys, unsorted := getColumnMap(fSlice, " ", k)
 	less := subSort(unsorted, reversal)
@@ -174,42 +230,72 @@ func sortByColumn(fSlice []string, k int, mainSort, subSort sliceSort, reversal 
 	return result
 }
 
-func sortSort(fSlice []string, k int, unique, numeric, reversal bool) []string {
+//Sort - функция, которая принимает слайс
+//и структуру с параметрами сортировки
+//и возвращает отсортированный слайс
+func Sort(fSlice []string, keys Keys) []string {
 	subSort := alphabetSort
 	mainSort := alphabetSort
-	if numeric {
+	if keys.Numeric {
 		mainSort = numSort
 	}
 
-	if unique {
-		fSlice = Unique(fSlice)
+	if keys.Unique {
+		fSlice = unique(fSlice)
 	}
 
-	if k >= 0 {
-		return sortByColumn(fSlice, k, mainSort, subSort, reversal)
+	if keys.Column >= 0 {
+		return sortByColumn(fSlice, keys.Column, mainSort, subSort, keys.Reverse)
 	}
 
-	less := mainSort(fSlice, reversal)
+	less := mainSort(fSlice, keys.Reverse)
 	sort.Slice(fSlice, less)
 	return fSlice
 }
 
+//Keys - настройки сортировки
+type Keys struct {
+	Column  int
+	Numeric bool
+	Reverse bool
+	Unique  bool
+}
+
 func main() {
-	fName := "test.txt"
-	fSlice, err := readFile(fName)
+	var keys Keys
+	//настройки сортировки - возможные флаги
+	flag.IntVar(&keys.Column, "k", -1, "column to sort by")
+	flag.BoolVar(&keys.Numeric, "n", false, "numerical sort")
+	flag.BoolVar(&keys.Reverse, "r", false, "reverse order")
+	flag.BoolVar(&keys.Unique, "u", false, "unique lines")
+	flag.Usage = usage
+	flag.Parse()
+
+	args := flag.Args()
+
+	if len(args) < 1 {
+		usage()
+		return
+	}
+	//входной файл
+	finName := args[0]
+	//выходной файл
+	var foutName string
+	if len(args) > 1 {
+		foutName = args[1]
+	}
+	//читаем входной файл
+	fSlice, err := ReadFile(finName)
 	if err != nil {
 		log.Fatalf("Error while reading file: %s", err)
 	}
-
-	k := 6
-	uniq := true
-	numeric := false
-	reverse := false
-
-	fSlice = sortSort(fSlice, k, uniq, numeric, reverse)
-
-	err = writeFile(fName, fSlice)
-
+	if keys.Column != -1 {
+		keys.Column--
+	}
+	//сортируем
+	fSlice = Sort(fSlice, keys)
+	//пишем выходной файл
+	err = WriteFile(foutName, fSlice)
 	if err != nil {
 		log.Fatalf("Error while writing file: %s", err)
 	}
--- develop/dev03/task_test.go
+++ develop/dev03/task_test.go
@@ -1,1 +1,108 @@
 package main
+
+import (
+	"strings"
+	"testing"
+)
+
+var defaultIn = "drwxr-xr-x 7 user user 4096 июн 10 2015 Sources\n" +
+	"drwxr-xr-x 8 user user 12288 янв 11 12:33 Pictures\n" +
+	"drwxr-xr-x 7 user user 4096 янв 13 11:42 Lightworks\n" +
+	"drwx------ 5 user user 12288 янв 15 14:59 Downloads\n" +
+	"drwxr-xr-x 7 user user 4096 окт 31 15:08 VirtualBox\n" +
+	"drwxr-xr-x 6 user user 4096 дек 6 14:29 Android"
+
+func TestReadFile(t *testing.T) {
+	expected := defaultIn
+	file := "test.txt"
+	fSlice, err := ReadFile(file)
+	if err != nil {
+		t.Fatalf("can't open file")
+	}
+	fileString := strings.Join(fSlice, "\n")
+	if fileString != expected {
+		t.Error("file wasn't read correctly")
+		t.Fatalf("expected %s\n, got %s", expected, fileString)
+	}
+}
+
+func TestSortNormal(t *testing.T) {
+	input := defaultIn
+	var keys Keys
+	inSlice := strings.Split(input, "\n")
+	sorted := Sort(inSlice, keys)
+	sortedString := strings.Join(sorted, "\n")
+	var expected = "drwx------ 5 user user 12288 янв 15 14:59 Downloads\n" +
+		"drwxr-xr-x 6 user user 4096 дек 6 14:29 Android\n" +
+		"drwxr-xr-x 7 user user 4096 июн 10 2015 Sources\n" +
+		"drwxr-xr-x 7 user user 4096 окт 31 15:08 VirtualBox\n" +
+		"drwxr-xr-x 7 user user 4096 янв 13 11:42 Lightworks\n" +
+		"drwxr-xr-x 8 user user 12288 янв 11 12:33 Pictures"
+	if sortedString != expected {
+		t.Error("a mistake in sorting")
+		t.Fatalf("expected %s\n, got %s", expected, sortedString)
+	}
+}
+
+func TestSortByColumn(t *testing.T) {
+	input := defaultIn
+	var keys Keys
+	keys.Column = 8
+	inSlice := strings.Split(input, "\n")
+	sorted := Sort(inSlice, keys)
+	sortedString := strings.Join(sorted, "\n")
+	var expected = "drwxr-xr-x 6 user user 4096 дек 6 14:29 Android\n" +
+		"drwx------ 5 user user 12288 янв 15 14:59 Downloads\n" +
+		"drwxr-xr-x 7 user user 4096 янв 13 11:42 Lightworks\n" +
+		"drwxr-xr-x 8 user user 12288 янв 11 12:33 Pictures\n" +
+		"drwxr-xr-x 7 user user 4096 июн 10 2015 Sources\n" +
+		"drwxr-xr-x 7 user user 4096 окт 31 15:08 VirtualBox"
+
+	if sortedString != expected {
+		t.Error("a mistake in sorting")
+		t.Fatalf("expected %s\n, got %s", expected, sortedString)
+	}
+}
+
+func TestSortNumeric(t *testing.T) {
+	input := defaultIn
+	var keys Keys
+	keys.Column = 6
+	keys.Numeric = true
+	inSlice := strings.Split(input, "\n")
+	sorted := Sort(inSlice, keys)
+	sortedString := strings.Join(sorted, "\n")
+	var expected = "drwxr-xr-x 6 user user 4096 дек 6 14:29 Android\n" +
+		"drwxr-xr-x 7 user user 4096 июн 10 2015 Sources\n" +
+		"drwxr-xr-x 8 user user 12288 янв 11 12:33 Pictures\n" +
+		"drwxr-xr-x 7 user user 4096 янв 13 11:42 Lightworks\n" +
+		"drwx------ 5 user user 12288 янв 15 14:59 Downloads\n" +
+		"drwxr-xr-x 7 user user 4096 окт 31 15:08 VirtualBox"
+
+	if sortedString != expected {
+		t.Error("a mistake in sorting")
+		t.Fatalf("expected %s\n, got %s", expected, sortedString)
+	}
+}
+
+func TestSortUnique(t *testing.T) {
+	input := defaultIn
+	var keys Keys
+	keys.Column = 8
+	keys.Unique = true
+	inSlice := strings.Split(input, "\n")
+	inSlice = append(inSlice, "drwx------ 5 user user 12288 янв 15 14:59 Downloads")
+	sorted := Sort(inSlice, keys)
+	sortedString := strings.Join(sorted, "\n")
+	var expected = "drwxr-xr-x 6 user user 4096 дек 6 14:29 Android\n" +
+		"drwx------ 5 user user 12288 янв 15 14:59 Downloads\n" +
+		"drwxr-xr-x 7 user user 4096 янв 13 11:42 Lightworks\n" +
+		"drwxr-xr-x 8 user user 12288 янв 11 12:33 Pictures\n" +
+		"drwxr-xr-x 7 user user 4096 июн 10 2015 Sources\n" +
+		"drwxr-xr-x 7 user user 4096 окт 31 15:08 VirtualBox"
+
+	if sortedString != expected {
+		t.Error("a mistake in sorting")
+		t.Fatalf("expected %s\n, got %s", expected, sortedString)
+	}
+}
