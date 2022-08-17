840ac84593246e1b7d3610d1da2138a2b81b6a8f
yura <yura11011@yandex.com>
2022-08-17 11:14:01 +0300

    task 07

--- develop/dev07/task.go
+++ develop/dev07/task.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"reflect"
+	"time"
+)
+
 /*
 === Or channel ===
 
@@ -33,6 +39,44 @@ start := time.Now()
 fmt.Printf(“fone after %v”, time.Since(start))
 */
 
-func main() {
+//очевидное решение на select
+//с рефлексией, чтобы определять количество каналов
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	or := make(chan interface{})
+	allCases := make([]reflect.SelectCase, len(channels))
+	for i, ch := range channels {
+		allCases[i] = reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(ch)}
+	}
+	ok := true
+	var value interface{}
+	for ok {
+		_, value, ok = reflect.Select(allCases)
+		if ok {
+			or <- value
+		}
+	}
+	close(or)
+	return or
+}
 
+func main() {
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	start := time.Now()
+	<-or(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Second),
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+	)
+
+	fmt.Printf("fone after %v", time.Since(start))
 }
