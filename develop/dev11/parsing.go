cf451261c4e41655694eb0cfefb7c88ff0b17f9d
yura <yura11011@yandex.com>
2022-08-21 17:07:17 +0300

    parsePost

--- develop/dev11/parsing.go
+++ develop/dev11/parsing.go
@@ -3,19 +3,19 @@ package main
 import (
 	"errors"
 	"net/http"
+	"net/url"
 	"strconv"
 	"time"
 )
 
-func parseGet(r *http.Request, getNum bool) (Event, error) {
-	query := r.URL.Query()
-	err := errors.New("parameter not found")
+func parseParams(getNum bool, values url.Values) (Event, error) {
 	var event Event
-	event.UserID = query.Get("user_id")
+	err := errors.New("parameter not found")
+	event.UserID = values.Get("user_id")
 	if event.UserID == "" {
 		return event, err
 	}
-	dateStr := query.Get("date")
+	dateStr := values.Get("date")
 	if dateStr == "" {
 		return event, err
 	}
@@ -27,10 +27,25 @@ func parseGet(r *http.Request, getNum bool) (Event, error) {
 	if !getNum {
 		return event, nil
 	}
-	numStr := query.Get("event_num")
+	numStr := values.Get("event_num")
 	if numStr == "" {
 		return event, err
 	}
 	event.Num, err = strconv.Atoi(numStr)
 	return event, err
+}
+
+func parseGet(r *http.Request, getNum bool) (Event, error) {
+	query := r.URL.Query()
+	return parseParams(getNum, query)
+}
+
+func parsePost(r *http.Request, getNum bool) (Event, error) {
+	var event Event
+	err := r.ParseForm()
+	if err != nil {
+		return event, err
+	}
+	values := r.PostForm
+	return parseParams(getNum, values)
 }
