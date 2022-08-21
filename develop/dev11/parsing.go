86f8eb4176282f646856830574f730fab8a188f3
yura <yura11011@yandex.com>
2022-08-21 17:20:43 +0300

    parsing post description

--- develop/dev11/parsing.go
+++ develop/dev11/parsing.go
@@ -8,44 +8,61 @@ import (
 	"time"
 )
 
-func parseParams(getNum bool, values url.Values) (Event, error) {
+func parseParams(getNum bool, getDescr bool, values url.Values) (Event, error) {
 	var event Event
-	err := errors.New("parameter not found")
-	event.UserID = values.Get("user_id")
-	if event.UserID == "" {
+	var err error
+	errString := " parameter not found"
+
+	param := "user_id"
+	if !values.Has(param) {
+		return event, errors.New(param + errString)
+	}
+	event.UserID = values.Get(param)
+
+	param = "date"
+	if !values.Has(param) {
+		return event, errors.New(param + errString)
+	}
+	dateStr := values.Get(param)
+	event.Date, err = time.Parse(dateForm, dateStr)
+	if err != nil {
 		return event, err
 	}
-	dateStr := values.Get("date")
-	if dateStr == "" {
-		return event, err
-	}
-	var errTime error
-	event.Date, errTime = time.Parse(dateForm, dateStr)
-	if errTime != nil {
-		return event, err
-	}
+
 	if !getNum {
 		return event, nil
 	}
-	numStr := values.Get("event_num")
-	if numStr == "" {
-		return event, err
+
+	param = "event_num"
+	if !values.Has(param) {
+		return event, errors.New(param + errString)
 	}
+	numStr := values.Get(param)
 	event.Num, err = strconv.Atoi(numStr)
-	return event, err
+
+	if !getDescr {
+		return event, nil
+	}
+
+	param = "description"
+	if !values.Has(param) {
+		return event, errors.New(param + errString)
+	}
+	event.Description = values.Get(param)
+	return event, nil
 }
 
-func parseGet(r *http.Request, getNum bool) (Event, error) {
+func parseGet(r *http.Request) (Event, error) {
 	query := r.URL.Query()
-	return parseParams(getNum, query)
+	return parseParams(false, false, query)
 }
 
-func parsePost(r *http.Request, getNum bool) (Event, error) {
+func parsePost(r *http.Request, getNum bool, getDescr bool) (Event, error) {
 	var event Event
 	err := r.ParseForm()
 	if err != nil {
 		return event, err
 	}
 	values := r.PostForm
-	return parseParams(getNum, values)
+	return parseParams(getNum, getDescr, values)
 }
