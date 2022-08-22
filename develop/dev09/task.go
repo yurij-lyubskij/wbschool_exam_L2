4874f115aeaf5204f925c86eebfe57211d529ab9
yura <yura11011@yandex.com>
2022-08-22 19:46:51 +0300

    get links example

--- develop/dev09/task.go
+++ develop/dev09/task.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"log"
+	"strings"
+)
+
 /*
 === Утилита wget ===
 
@@ -9,5 +16,30 @@ package main
 */
 
 func main() {
-
+	//resp, err := http.Get("http://example.com/")
+	//if err != nil {
+	//	// handle error
+	//}
+	//defer resp.Body.Close()
+	//body, err := io.ReadAll(resp.Body)
+	s := `<p>Links:</p><ul><li><a href="foo">Foo</a><li><a href="/bar/baz">BarBaz</a></ul>`
+	doc, err := html.Parse(strings.NewReader(s))
+	if err != nil {
+		log.Fatal(err)
+	}
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode {
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+					fmt.Println(a.Val)
+					break
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(doc)
 }
